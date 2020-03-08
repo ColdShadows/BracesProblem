b99876a50aba9fe04d4b808f56ae8374da31f106
Travis Kean <traviskean@nexsystech.com>
2020-03-08 17:25:17 -0400

    Update project from console application to class library. TDD wrote the "IsProperBraces" function to determine if a string has proper open and closing braces.

--- BracesProblem/StringEvaluator/StringEvaluator.cs
+++ BracesProblem/StringEvaluator/StringEvaluator.cs
@@ -1,7 +1,37 @@
-﻿namespace StringEvaluator
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace StringEvaluator
 {
     public class StringEvaluator
     {
+        public static bool IsProperBraces(string input)
+        {
+            var letterStack = new Stack<char>();
+            var openBraces = new Dictionary<char, char> { { '{', '}' }, { '[', ']' }, { '(', ')' } };
 
+            foreach(var letter in input)
+            {
+                if(openBraces.ContainsKey(letter))
+                {
+                    letterStack.Push(letter);
+                }
+                else if (openBraces.ContainsValue(letter) && letter == openBraces.GetValueOrDefault(letterStack.Peek()))
+                {
+                    letterStack.Pop();
+                }
+                else if (openBraces.ContainsValue(letter) &&  letter != openBraces.GetValueOrDefault(letterStack.Peek()))
+                {
+                    return false;
+                }
+            }
+
+            if(letterStack.Any())
+            { 
+                return false;
+            }
+
+            return true;
+        }
     }
 }
--- BracesProblem/StringEvaluatorTests/StringEvaluatorTests.cs
+++ BracesProblem/StringEvaluatorTests/StringEvaluatorTests.cs
@@ -1,6 +1,20 @@
-﻿namespace StringEvaluatorTests
-{
-    class StringEvaluatorTests
+﻿using Xunit;
+
+namespace StringEvaluator
+{    
+    public class StringEvaluatorTests
     {
+        [Theory]
+        [InlineData("{{}", false)]
+        [InlineData("{{}}", true)]
+        [InlineData("{[{}]}", true)]
+        [InlineData("{[{]}}", false)]
+        [InlineData("{d{}ssl}erw", true)]
+        public void IsProperBraces_InputString_ReturnsExpectedBoolean(string input, bool expected)
+        {
+            var result = StringEvaluator.IsProperBraces(input);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
